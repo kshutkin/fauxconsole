dac5540502961946929ec80fe07b2dd2c630fd84
Konstantin Shutkin <kostya80@gmail.com>
2012-02-23 04:18:59 +0400

    fauxconsole as AMD module

---
+++ fauxconsole.js
@@ -0,0 +1,35 @@
+﻿/* Faux Console by Chris Heilmann http://wait-till-i.com */
+define("console", [ "require" ], function(require) {
+
+	if (!!window.console) {
+		return window.console;
+	}
+
+	var tmpl = '<a href="javascript:console.hide()">close</a><a href="javascript:console.clear()">clear</a>';
+	var d = document.createElement('div');
+	d.innerHTML = tmpl;
+
+	var id = 'fauxconsole';
+	if (!document.getElementById(id)) {
+		d.id = id;
+	}
+
+	window.console = {
+		log : function(o) {
+			d.innerHTML += '<br/>' + o;
+			d.style.display = 'block';
+		},
+		hide : function() {
+			d.style.display = 'none';
+		},
+		clear : function() {
+			d.innerHTML = tmpl;
+		}
+	}
+
+	require([ "domReady!", "css!fauxconsole" ], function() {
+		document.body.appendChild(d);
+	});
+
+	return window.console;
+});
